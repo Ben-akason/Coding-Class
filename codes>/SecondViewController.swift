053aea900af42803640d81770d7dc7cb901e00f4
Akason <akason@Akasons-MacBook-Pro.local>
2018-07-16 15:56:19 -0500

    new project

---
+++ codes>/SecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.swift
+//  codes>
+//
+//  Created by Akason on 7/16/18.
+//  Copyright © 2018 Akason. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
